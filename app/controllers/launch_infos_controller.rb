6a42add5eaf3f974ff16f7f3cbbe3d49294bbc5f
ElonKerman <blakeablin@icloud.com>
2018-08-30 19:02:50 -0500

    finish launch info

--- app/controllers/launch_infos_controller.rb
+++ app/controllers/launch_infos_controller.rb
@@ -2,7 +2,7 @@ require "open-uri"
 require "json"
 class LaunchInfosController < ApplicationController
   def index
-    @data = open("https://launchlibrary.net/1.4/launch/next/10").read
+    @data = open("https://launchlibrary.net/1.4/launch/next/15").read
     @launches = JSON.parse(@data)["launches"]
     @launch_infos = LaunchInfo.order(:date_time => :asc)
     render('index.html.erb')
