63d0d40cf64cb76d2c2f3251d21c86206723cee5
ElonKerman <blakeablin@icloud.com>
2018-09-11 14:22:20 -0500

    update 1

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,17 @@ Bundler.require(*Rails.groups)
 module Connectspacekids
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'Connectspacekids.herokuapp.com',
+      user_name:            ENV["email"],
+      password:             "something",
+      authentication:       'plain',
+      enable_starttls_auto: true }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
