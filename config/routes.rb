2552ae0205698a5743bde9494d8ccc7931a41f1c
ElonKerman <blakeablin@icloud.com>
2018-10-23 19:49:00 -0500

    check your spam

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users#, controllers: {
+  #  sessions: 'users/sessions'
+  #}
+
   get("/launchinfo", {:controller=>"launch_infos", :action=>"index"})
   root 'launch_infos#index'
 
